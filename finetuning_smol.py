9d2dc488adf3fa3d206944d459b752e16f8e1e11
axeld5 <axel.darmouni@student-cs.fr>
2024-12-28 02:28:21 +0100

    testiing smol

--- finetuning_smol.py
+++ finetuning_smol.py
@@ -59,44 +59,6 @@ def format_data(sample):
 train_ds = Dataset.from_dict(json.load(open("training_data/training_dict.json")))
 train_dataset = [format_data(sample) for sample in train_ds]
 
-def generate_text_from_sample(model, processor, sample, max_new_tokens=1024, device="cuda"):
-    # Prepare the text input by applying the chat template
-    text_input = processor.apply_chat_template(
-        sample[1:2],  # Use the sample without the system message
-        add_generation_prompt=True
-    )
-
-    image_inputs = []
-    image = Image.open(sample[1]['content'][0]['image'])
-    if image.mode != 'RGB':
-        image = image.convert('RGB')
-    image_inputs.append([image])
-
-    # Prepare the inputs for the model
-    model_inputs = processor(
-        #text=[text_input],
-        text=text_input,
-        images=image_inputs,
-        return_tensors="pt",
-    ).to(device)  # Move inputs to the specified device
-
-    # Generate text with the model
-    generated_ids = model.generate(**model_inputs, max_new_tokens=max_new_tokens)
-
-    # Trim the generated ids to remove the input ids
-    trimmed_generated_ids = [
-        out_ids[len(in_ids):] for in_ids, out_ids in zip(model_inputs.input_ids, generated_ids)
-    ]
-
-    # Decode the output text
-    output_text = processor.batch_decode(
-        trimmed_generated_ids,
-        skip_special_tokens=True,
-        clean_up_tokenization_spaces=False
-    )
-
-    return output_text[0]
-
 # Configure LoRA
 peft_config = LoraConfig(
     r=8,
@@ -142,7 +104,6 @@ image_token_id = processor.tokenizer.additional_special_tokens_ids[
 
 def collate_fn(examples):
     texts = [processor.apply_chat_template(example, tokenize=False) for example in examples]
-
     image_inputs = []
     for example in examples:
       image = Image.open(example[1]['content'][0]['image'])
---
+++ inference_smol.py
@@ -0,0 +1,107 @@
+
+import torch
+import json
+from transformers import Idefics3ForConditionalGeneration, AutoProcessor
+from dotenv import load_dotenv
+from datasets import Dataset
+from PIL import Image
+import pandas as pd
+from datasets import load_dataset
+
+load_dotenv()
+model_id = "HuggingFaceTB/SmolVLM-Instruct"
+model = Idefics3ForConditionalGeneration.from_pretrained(
+    model_id,
+    device_map="auto",
+    torch_dtype=torch.bfloat16,
+    _attn_implementation="flash_attention_2",
+)
+
+processor = AutoProcessor.from_pretrained(model_id)
+
+system_message = """You are a Vision Language Model specialized in handling mathematical questions with reasoning
+Your task is, given a question that requires thinking, to give the right answer to it."""
+
+def format_data(sample):
+    return [
+        {
+            "role": "system",
+            "content": [
+                {
+                    "type": "text",
+                    "text": system_message
+                }
+            ],
+        },
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image",
+                    "image": sample["image"],
+                },
+                {
+                    "type": "text",
+                    "text": sample['input'],
+                }
+            ],
+        },
+    ]
+
+train_ds = Dataset.from_dict(json.load(open("training_data/training_dict.json")))
+train_dataset = [format_data(sample) for sample in train_ds]
+
+def generate_text_from_sample(model, processor, sample, max_new_tokens=1024, device="cuda"):
+    # Prepare the text input by applying the chat template
+    text_input = processor.apply_chat_template(
+        sample[1:2],  # Use the sample without the system message
+        add_generation_prompt=True
+    )
+
+    image_inputs = []
+    image = Image.open(sample[1]['content'][0]['image'])
+    if image.mode != 'RGB':
+        image = image.convert('RGB')
+    image_inputs.append([image])
+
+    # Prepare the inputs for the model
+    model_inputs = processor(
+        #text=[text_input],
+        text=text_input,
+        images=image_inputs,
+        return_tensors="pt",
+    ).to(device)  # Move inputs to the specified device
+
+    # Generate text with the model
+    generated_ids = model.generate(**model_inputs, max_new_tokens=max_new_tokens)
+
+    # Trim the generated ids to remove the input ids
+    trimmed_generated_ids = [
+        out_ids[len(in_ids):] for in_ids, out_ids in zip(model_inputs.input_ids, generated_ids)
+    ]
+
+    # Decode the output text
+    output_text = processor.batch_decode(
+        trimmed_generated_ids,
+        skip_special_tokens=True,
+        clean_up_tokenization_spaces=False
+    )
+
+    return output_text[0]
+
+dataset = load_dataset("AI4Math/MathVista")
+prompts = []
+result_list = []
+true_answers = []
+for i in range(351, len(dataset["testmini"])):
+    print(f"step {i - 351}")
+    prompt = dataset["testmini"][i]["query"]
+    raw_image = dataset["testmini"][i]["decoded_image"]
+    inputs = format_data(sample = {"image": raw_image, "input": prompt})
+    output = generate_text_from_sample(model, processor, inputs)
+    print(output)
+    prompts.append(prompt)
+    result_list.append(output)
+    true_answers.append(dataset["testmini"][i]["answer"])
+df = pd.DataFrame({"prompt": prompts, "result": result_list, "true_answer": true_answers})
+df.to_csv("outputs/results_smol.csv", index=False)
